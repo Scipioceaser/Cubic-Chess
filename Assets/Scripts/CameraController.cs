2bd2ae9c22b6a3a046f2dffed1a92891b6b91e8e
Scipioceaser <awnagel700@gmail.com>
2019-09-27 21:08:23 +0200

    Added camera orbiting system.

--- Assets/Scripts/CameraController.cs
+++ Assets/Scripts/CameraController.cs
@@ -4,21 +4,34 @@ using UnityEngine;
 
 public class CameraController : MonoBehaviour
 {
-    public float cameraDistance = 5f;
-
-    private Node[,,] sceneNodes;
+    [Header("Camera controls")]
+    public float mouseRotationSpeed = 2f;
+    public float rotateSmoothness = 0.5f;
+    public float maxCameraDistance = 10f;
+    public float minCameraDistance = .5f;
+    public float mouseYMinLimit = -360f;
+    public float mouseYMaxLimit = 360f;
+    
     private Map mapObject;
     private Camera camera;
     private Vector3 mapCentre;
-    [SerializeField]
-    private List<Vector3> cameraMoveSphere = new List<Vector3>();
+    private Vector3 cameraOffset;
+    private float cameraDistance = 5f;
 
+    private float mouseX;
+    private float mouseY;
+    
     // Start is called before the first frame update
     void Start()
     {
         mapObject = GameObject.FindGameObjectWithTag("Map").GetComponent<Map>();
         camera = GetComponent<Camera>();
-        transform.position = (mapCentre + new Vector3(2.5f, 10f, -cameraDistance));
+        
+        Vector3 angles = transform.eulerAngles;
+        mouseX = angles.x;
+        mouseY = angles.y;
+
+        SetCentrePosition();
     }
     
     public void SetCentrePosition()
@@ -26,5 +39,41 @@ public class CameraController : MonoBehaviour
         Vector3 nPos = new Vector3((mapObject.nodeScale / 2f), (mapObject.nodeScale / 2f), (mapObject.nodeScale / 2f));
         Vector3 mPos = new Vector3((mapObject.boardSize + 1) / 2, (mapObject.boardHeight + 1) / 2, (mapObject.boardSize + 1) / 2);
         mapCentre = mPos + nPos;
+        cameraOffset = transform.position - mapCentre;
+    }
+
+    private void LateUpdate()
+    {
+        OrbitCamera();
+    }
+
+    private void OrbitCamera()
+    {
+        if (Input.GetMouseButton(1))
+        {
+            mouseX += Input.GetAxis("Mouse X") * mouseRotationSpeed * cameraDistance * rotateSmoothness;
+            mouseY -= Input.GetAxis("Mouse Y") * mouseRotationSpeed * rotateSmoothness * cameraDistance;
+
+            mouseY = ClampAngle(mouseY, mouseYMinLimit, mouseYMaxLimit);
+
+            Quaternion r = Quaternion.Euler(mouseY, mouseX, 0);
+
+            cameraDistance = Mathf.Clamp(cameraDistance * 5, minCameraDistance, maxCameraDistance);
+
+            Vector3 negcameraDistance = new Vector3(0, 0, -cameraDistance);
+            Vector3 position = r * negcameraDistance + mapCentre;
+
+            transform.position = position;
+            transform.rotation = r;
+        }
+    }
+
+    public static float ClampAngle(float angle, float min, float max)
+    {
+        if (angle < -360f)
+            angle += 360f;
+        if (angle > 360f)
+            angle -= 360f;
+        return Mathf.Clamp(angle, min, max);
     }
 }
